1076cb36ed5879ab22c9a07dc75dd452ebe6cc52
Daniel San <danielgdt88@hotmail.com>
2024-06-21 20:29:43 -0300

    img responsiva js

--- js/imgMobile.js
+++ js/imgMobile.js
@@ -1,6 +1,12 @@
 export default function imgMobile() {
   window.addEventListener("load", () => {
     const imgDesktop = document.querySelector("[data-img]");
-    console.log(imgDesktop);
+    const imgMobileJs = "./images/bg-sidebar-mobile.svg";
+
+    const mediaQueryJs = matchMedia("(max-width: 850px)").matches;
+
+    if (mediaQueryJs) {
+      imgDesktop.setAttribute("src", imgMobileJs);
+    }
   });
 }
