7bba9d548309b6b8a437966360db7a89a5a45704
Daniel San <danielgdt88@hotmail.com>
2024-06-22 17:49:38 -0300

    mudança de img responsivo

--- js/imgMobile.js
+++ js/imgMobile.js
@@ -1,11 +1,16 @@
 export default function imgMobile() {
-  const mediaQueryJs = matchMedia("(max-width: 810px)").matches;
   window.addEventListener("load", () => {
-    const imgDesktop = document.querySelector("[data-img]");
-    const imgMobileJs = "./images/bg-sidebar-mobile.svg";
+    window.addEventListener("resize", () => {
+      const mediaQueryJs = matchMedia("(max-width: 810px)").matches;
+      const imgDesktop = document.querySelector("[data-img]");
+      const imgMobileJs = "./images/bg-sidebar-mobile.svg";
+      const imgDesktopJs = "./images/bg-sidebar-desktop.svg";
 
-    if (mediaQueryJs) {
-      imgDesktop.setAttribute("src", imgMobileJs);
-    }
+      if (mediaQueryJs) {
+        imgDesktop.setAttribute("src", imgMobileJs);
+      } else {
+        imgDesktop.setAttribute("src", imgDesktopJs);
+      }
+    });
   });
 }
