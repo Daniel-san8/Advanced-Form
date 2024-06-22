611c4e3751ea6e80a95ad3c4cdfe4deb0758c523
Daniel San <danielgdt88@hotmail.com>
2024-06-22 20:19:18 -0300

    validação de tel feita

--- js/validationInputs.js
+++ js/validationInputs.js
@@ -1,15 +1,26 @@
 export default function validationInputs() {
   const inputEmail = document.querySelector('[data-input="email"]');
+  const inputTel = document.querySelector('[data-input="tel"]');
   const rgxEmail = /^\S+@\S+\.\S+$/;
-
+  const rgxTel = /^\d{10}$/;
   inputEmail.addEventListener("change", () => {
     if (rgxEmail.test(inputEmail.value)) {
-      console.log("VALIDADO!!");
       inputEmail.classList.remove("inputInvalidateJs");
       inputEmail.classList.add("inputValidateJs");
     } else {
       inputEmail.classList.remove("inputValidateJs");
       inputEmail.classList.add("inputInvalidateJs");
+    }
+  });
+
+  inputTel.addEventListener("change", () => {
+    if (rgxTel.test(inputTel.value)) {
+      console.log("VALIDADO!!");
+      inputTel.classList.remove("inputInvalidateJs");
+      inputTel.classList.add("inputValidateJs");
+    } else {
+      inputTel.classList.remove("inputValidateJs");
+      inputTel.classList.add("inputInvalidateJs");
     }
   });
 }
