89a116961b590c49dbc915eb9d4ebbbb778b855f
Daniel San <danielgdt88@hotmail.com>
2024-06-22 19:53:07 -0300

    botão de sumiço feito

--- js/mudancaDeEvent.js
+++ js/mudancaDeEvent.js
@@ -1,15 +1,10 @@
 export default function mudancaDeEvent() {
   window.addEventListener("load", () => {
-    let widthMedia = matchMedia("(max-width: 810px)").matches;
-    window.addEventListener("resize", () => {
-      const buttonElement = document.querySelector("[data-button]");
-      buttonElement.addEventListener("click", (event) => {
-        event.preventDefault();
-        const elementForm1 = document.querySelector('[data-form="1"');
-        console.log(elementForm1);
-      });
-      if (widthMedia) {
-      }
+    const buttonElement = document.querySelector("[data-button]");
+    buttonElement.addEventListener("click", (event) => {
+      event.preventDefault();
+      const elementForm1 = document.querySelector('[data-form="1"');
+      elementForm1.classList.add("noneJs");
     });
   });
 }
