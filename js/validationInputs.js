bce0caadaaf4d2fb0818a17bfd205af850faae57
Daniel San <danielgdt88@hotmail.com>
2024-06-22 20:54:58 -0300

    adição do field required

--- js/validationInputs.js
+++ js/validationInputs.js
@@ -1,25 +1,30 @@
 export default function validationInputs() {
   const inputEmail = document.querySelector('[data-input="email"]');
   const inputTel = document.querySelector('[data-input="tel"]');
+  const labelEmail = document.querySelector("[data-label='email']");
+  const labelTel = document.querySelector("[data-label='tel']");
   const rgxEmail = /^\S+@\S+\.\S+$/;
-  const rgxTel = /^\d{10}$/;
+  const rgxTel = /^\d{9}$/;
   inputEmail.addEventListener("change", () => {
     if (rgxEmail.test(inputEmail.value)) {
+      labelEmail.classList.remove("labelRequired");
       inputEmail.classList.remove("inputInvalidateJs");
       inputEmail.classList.add("inputValidateJs");
     } else {
       inputEmail.classList.remove("inputValidateJs");
       inputEmail.classList.add("inputInvalidateJs");
+      labelEmail.classList.add("labelRequired");
     }
   });
 
   inputTel.addEventListener("change", () => {
     if (rgxTel.test(inputTel.value)) {
-      console.log("VALIDADO!!");
+      labelTel.classList.remove("labelRequired");
       inputTel.classList.remove("inputInvalidateJs");
       inputTel.classList.add("inputValidateJs");
     } else {
       inputTel.classList.remove("inputValidateJs");
+      labelTel.classList.add("labelRequired");
       inputTel.classList.add("inputInvalidateJs");
     }
   });
