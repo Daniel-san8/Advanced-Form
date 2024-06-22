025958b55883713f36308e23aedac6c230b26218
Daniel San <danielgdt88@hotmail.com>
2024-06-22 19:49:22 -0300

    primeiros passos para o js DOM do step 1

--- js/imgMobile.js
+++ js/imgMobile.js
@@ -1,18 +1,16 @@
 export default function imgMobile() {
-  window.addEventListener("DOMContentLoaded", () => {
-    window.addEventListener("load", () => {
-      window.addEventListener("resize", () => {
-        const mediaQueryJs = matchMedia("(max-width: 810px)").matches;
-        const imgDesktop = document.querySelector("[data-img]");
-        const imgMobileJs = "./images/bg-sidebar-mobile.svg";
-        const imgDesktopJs = "./images/bg-sidebar-desktop.svg";
+  window.addEventListener("load", () => {
+    window.addEventListener("resize", () => {
+      const mediaQueryJs = matchMedia("(max-width: 810px)").matches;
+      const imgDesktop = document.querySelector("[data-img]");
+      const imgMobileJs = "./images/bg-sidebar-mobile.svg";
+      const imgDesktopJs = "./images/bg-sidebar-desktop.svg";
 
-        if (mediaQueryJs) {
-          imgDesktop.setAttribute("src", imgMobileJs);
-        } else {
-          imgDesktop.setAttribute("src", imgDesktopJs);
-        }
-      });
+      if (mediaQueryJs) {
+        imgDesktop.setAttribute("src", imgMobileJs);
+      } else {
+        imgDesktop.setAttribute("src", imgDesktopJs);
+      }
     });
   });
 }
