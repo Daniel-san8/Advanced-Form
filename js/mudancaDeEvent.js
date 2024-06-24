4151df541ff534a7502bd91bccf0878bb765861d
Daniel San <danielgdt88@hotmail.com>
2024-06-24 17:51:31 -0300

    primeira funcionalidade criada

--- js/mudancaDeEvent.js
+++ js/mudancaDeEvent.js
@@ -3,12 +3,36 @@ export default function mudancaDeEvent() {
   const elementForm1 = document.querySelector('[data-form="1"');
   const elementForm2 = document.querySelector('[data-form="2"]');
   const numbers = document.querySelectorAll('[data-numbers="number"]');
-  let count = 0;
   window.addEventListener("load", () => {
     buttonElement.addEventListener("click", (event) => {
       event.preventDefault();
-      elementForm1.classList.add("noneJs");
-      console.log(numbers[count]);
+      // elementForm1.classList.add("noneJs");
     });
+  });
+
+  function captaNumbers(e) {
+    let number = Number(e.target.innerHTML);
+    if (number === 1) {
+      elementForm1.classList.remove("noneJs");
+      elementForm2.classList.add("noneJs");
+    } else if (number === 2) {
+      elementForm1.classList.add("noneJs");
+      elementForm2.classList.remove("noneJs");
+    }
+    // } else if (number === 3) {
+    //   elementForm1.classList.add("noneJs");
+    //   elementForm2.classList.add("noneJs");
+    //   elementForm3.classList.remove("noneJs");
+    //   elementForm4.classList.add("noneJs");
+    // } else if (number === 4) {
+    //   elementForm1.classList.add("noneJs");
+    //   elementForm2.classList.add("noneJs");
+    //   elementForm3.classList.add("noneJs");
+    //   elementForm4.classList.remove("noneJs");
+    // }
+  }
+
+  numbers.forEach((number) => {
+    number.addEventListener("click", captaNumbers);
   });
 }
