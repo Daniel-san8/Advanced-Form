5cb37b93893c2b94167512cc0415d2396de9d147
Daniel San <danielgdt88@hotmail.com>
2024-06-23 21:17:23 -0300

    refatoração de código

--- js/imgMobile.js
+++ js/imgMobile.js
@@ -1,10 +1,14 @@
 export default function imgMobile() {
   window.addEventListener("load", () => {
+    const imgDesktop = document.querySelector("[data-img]");
+    const imgMobileJs = "./images/bg-sidebar-mobile.svg";
+    const imgDesktopJs = "./images/bg-sidebar-desktop.svg";
+
+    if (innerWidth <= 810) {
+      imgDesktop.setAttribute("src", imgMobileJs);
+    }
     window.addEventListener("resize", () => {
       const mediaQueryJs = matchMedia("(max-width: 810px)").matches;
-      const imgDesktop = document.querySelector("[data-img]");
-      const imgMobileJs = "./images/bg-sidebar-mobile.svg";
-      const imgDesktopJs = "./images/bg-sidebar-desktop.svg";
 
       if (mediaQueryJs) {
         imgDesktop.setAttribute("src", imgMobileJs);
