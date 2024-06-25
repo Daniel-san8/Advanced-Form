7308da347bb28893631dc0ad6404bed2dbd614e0
Daniel San <danielgdt88@hotmail.com>
2024-06-24 21:22:29 -0300

    efeito do toggle

--- js/eventToggle.js
+++ js/eventToggle.js
@@ -4,7 +4,9 @@ export default function eventToggle() {
   elementToggle.addEventListener("click", () => {
     if (elementToggleLabel.classList.contains("ativo")) {
       elementToggleLabel.classList.remove("ativo");
+      elementToggleLabel.classList.add("desativado");
     } else {
+      elementToggleLabel.classList.remove("desativado");
       elementToggleLabel.classList.add("ativo");
     }
   });
