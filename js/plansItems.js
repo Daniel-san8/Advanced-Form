844231406ed744b497b48c0014925f6d9f4bae41
Daniel San <danielgdt88@hotmail.com>
2024-06-25 16:50:08 -0300

    função dos planos feita

--- js/plansItems.js
+++ js/plansItems.js
@@ -1,7 +1,22 @@
 export default function plansItems() {
   const plansItems = document.querySelectorAll('[data-span="plan-time"]');
   const toggleElement = document.querySelector(".toggleElementJs");
-  function plans() {
-    plansItems.forEach((plan) => {});
+  function plans(eventToggle) {
+    if (!eventToggle.target.classList.contains("ativo")) {
+      plansItems.forEach((plan) => {
+        plan.classList.add("ativo");
+        plansItems[2].innerHTML = "$90/yr";
+        plansItems[1].innerHTML = "$120/yr";
+        plansItems[0].innerHTML = "$150/yr";
+      });
+    } else {
+      plansItems.forEach((plan) => {
+        plan.classList.remove("ativo");
+        plansItems[2].innerHTML = "$9/mo";
+        plansItems[1].innerHTML = "$12/mo";
+        plansItems[0].innerHTML = "$15/mo";
+      });
+    }
   }
+  toggleElement.addEventListener("click", plans);
 }
