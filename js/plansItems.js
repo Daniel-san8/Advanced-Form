d21901046b39413649a2e7e122aab37c0f134f0c
Daniel San <danielgdt88@hotmail.com>
2024-06-25 17:06:29 -0300

    estilo simples no after do plan

--- js/plansItems.js
+++ js/plansItems.js
@@ -5,16 +5,16 @@ export default function plansItems() {
     if (!eventToggle.target.classList.contains("ativo")) {
       plansItems.forEach((plan) => {
         plan.classList.add("ativo");
-        plansItems[2].innerHTML = "$90/yr";
-        plansItems[1].innerHTML = "$120/yr";
-        plansItems[0].innerHTML = "$150/yr";
+        plansItems[2].textContent = "$90/yr";
+        plansItems[1].textContent = "$120/yr";
+        plansItems[0].textContent = "$150/yr";
       });
     } else {
       plansItems.forEach((plan) => {
         plan.classList.remove("ativo");
-        plansItems[2].innerHTML = "$9/mo";
-        plansItems[1].innerHTML = "$12/mo";
-        plansItems[0].innerHTML = "$15/mo";
+        plansItems[2].textContent = "$9/mo";
+        plansItems[1].textContent = "$12/mo";
+        plansItems[0].textContent = "$15/mo";
       });
     }
   }
