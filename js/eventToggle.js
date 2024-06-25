2dc946651149fcb845296383ec90dabfc3226be4
Daniel San <danielgdt88@hotmail.com>
2024-06-24 22:05:19 -0300

    animação do toggle completa

--- js/eventToggle.js
+++ js/eventToggle.js
@@ -1,13 +1,19 @@
 export default function eventToggle() {
   const elementToggle = document.querySelector(".toggleElement");
   const elementToggleLabel = document.querySelector(".toggleElementJs");
+  const monthly = document.querySelector("[data-monthly]");
+  const yearly = document.querySelector(".yearly");
   elementToggle.addEventListener("click", () => {
     if (elementToggleLabel.classList.contains("ativo")) {
       elementToggleLabel.classList.remove("ativo");
       elementToggleLabel.classList.add("desativado");
+      monthly.classList.remove("desativado");
+      yearly.classList.remove("ativo");
     } else {
       elementToggleLabel.classList.remove("desativado");
       elementToggleLabel.classList.add("ativo");
+      monthly.classList.add("desativado");
+      yearly.classList.add("ativo");
     }
   });
 }
