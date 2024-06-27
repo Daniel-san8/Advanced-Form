35d30a5266a3f3128998eed03219b773f14c4df6
Daniel San <danielgdt88@hotmail.com>
2024-06-27 17:54:17 -0300

    att

--- js/imgMobile.js
+++ js/imgMobile.js
@@ -4,11 +4,11 @@ export default function imgMobile() {
     const imgMobileJs = "./images/bg-sidebar-mobile.svg";
     const imgDesktopJs = "./images/bg-sidebar-desktop.svg";
 
-    if (innerWidth <= 810) {
+    if (innerWidth <= 825) {
       imgDesktop.setAttribute("src", imgMobileJs);
     }
     window.addEventListener("resize", () => {
-      const mediaQueryJs = matchMedia("(max-width: 810px)").matches;
+      const mediaQueryJs = matchMedia("(max-width: 825px)").matches;
 
       if (mediaQueryJs) {
         imgDesktop.setAttribute("src", imgMobileJs);
