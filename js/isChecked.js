5fc5ee2b5d6ba60dd59650254b5da4045370f2d7
Daniel San <danielgdt88@hotmail.com>
2024-06-27 21:34:13 -0300

    função feita

--- js/isChecked.js
+++ js/isChecked.js
@@ -2,6 +2,12 @@ export default function isChecked() {
   const inputsChecks = document.querySelectorAll("[data-check]");
   const typeGame = document.querySelectorAll('[data-type="game"]');
   inputsChecks.forEach((input) => {
-    input.addEventListener("click", (event) => {});
+    input.addEventListener("click", (event) => {
+      if (input.checked) {
+        input.parentElement.classList.add("checkedStyle");
+      } else {
+        input.parentElement.classList.remove("checkedStyle");
+      }
+    });
   });
 }
