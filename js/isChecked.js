8d52c6ea4b09992370fdb1019dcf643499613b92
Daniel San <danielgdt88@hotmail.com>
2024-07-15 14:00:01 -0300

    iniciando function de mudança do plano item

--- js/isChecked.js
+++ js/isChecked.js
@@ -1,6 +1,5 @@
 export default function isChecked() {
   const inputsChecks = document.querySelectorAll("[data-check]");
-  const typeGame = document.querySelectorAll('[data-type="game"]');
   inputsChecks.forEach((input) => {
     input.addEventListener("click", (event) => {
       if (input.checked) {
--- js/nextAndBack.js
+++ js/nextAndBack.js
@@ -1,13 +1,34 @@
 export default function nextAndBack() {
   const finish = document.querySelector(".item-finish");
   const finish2 = document.querySelector(".item-finish2");
+  const typeGame = document.querySelectorAll('[data-type="game"]');
   const plano = document.querySelector("[data-plano-ok]");
   let labelsPlans = document.querySelectorAll(".plans-items");
+  const novoElemento = `
+    <div class="option-item-finish">
+                        <span class="item-finish">Larger storage</span>
+                        <span class="item-finish2">+$20/yr</span>
+                    </div>
+  `;
+
+  typeGame.forEach((game) => {
+    game.addEventListener("click", (e) => {
+      let divGame = e.currentTarget;
+      let active = divGame.classList.contains("checkedStyle");
+      switch (divGame) {
+        case divGame:
+          break;
+
+        default:
+          break;
+      }
+    });
+  });
 
   labelsPlans.forEach((labelPlan) => {
     labelPlan.addEventListener("click", (e) => {
       let planoOk = e.currentTarget.children[1].innerHTML;
-      console.log(planoOk);
+      console.log(typeGame);
       switch (planoOk) {
         case "Arcade":
           plano.innerHTML = "Arcade";
